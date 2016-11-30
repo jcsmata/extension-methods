26ca15ffe51e23780b59701a231bab8e2148db83
João Mata <joaomata@gmail.com>
2016-11-30 01:17:57 +0000

    Created solution and unit tests

---
+++ ExtensionMethodsTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionMethodsTest
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
